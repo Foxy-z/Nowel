da629ff98ef95e68a43d0e85db99c68ad51475c8
TheFoxy41 <thefoxy41@gmail.com>
2018-11-25 20:51:34 +0100

    Initial commit

---
+++ src/fr/onecraft/nowel/Nowel.java
@@ -0,0 +1,24 @@
+package fr.onecraft.nowel;
+
+import fr.onecraft.nowel.commands.CmdIndice;
+import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Nowel extends JavaPlugin {
+    public static String PREFIX = "§9Noël > §7";
+    public static String ERROR = "§cErreur > §7";
+
+    @Override
+    public void onEnable() {
+        // register command
+        PluginCommand command = this.getCommand("indice");
+        command.setExecutor(new CmdIndice());
+        Bukkit.getConsoleSender().sendMessage("[" + getName() + "] Nowel has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("[" + getName() + "] Nowel has been disabled.");
+    }
+}
---
+++ src/fr/onecraft/nowel/commands/CmdIndice.java
@@ -0,0 +1,88 @@
+package fr.onecraft.nowel.commands;
+
+import fr.onecraft.nowel.Nowel;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.text.SimpleDateFormat;
+
+public class CmdIndice implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        // send error if command sender is not a player
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(Nowel.ERROR + "Cette commande n'est faisable que par un joueur.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        TextComponent link = new TextComponent("ici");
+        link.setColor(ChatColor.GREEN);
+        link.setBold(true);
+        link.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(
+                "§7Clique ici pour obtenir l'indice !"
+        ).create()));
+
+        String currentDayStr = new SimpleDateFormat("dd").format(System.currentTimeMillis());
+
+        if (args.length == 0) {
+            // add web redirect
+            link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
+                    "http://xmas.onecraft.fr/" + currentDayStr + ".php?uuid=" + player.getUniqueId().toString()
+            ));
+
+            player.sendMessage(Nowel.PREFIX + "Tu peux faire §a/indice <jour>§7 pour voir les anciens.");
+        } else {
+            int day;
+            int currentDay;
+            // parse string to int or else send error
+            try {
+                day = Integer.parseInt(args[0]);
+                currentDay = Integer.parseInt(currentDayStr);
+            } catch (NumberFormatException e) {
+                player.sendMessage(Nowel.ERROR + "Ce jour n'est pas valide.");
+                return true;
+            }
+
+            // return error if the date is not valid
+            if (day < 1) {
+                player.sendMessage(Nowel.ERROR + "Ce jour n'est pas valide.");
+                return true;
+            }
+
+            // return error if the date is not valid
+            if (day > 31) {
+                player.sendMessage(Nowel.ERROR + "T'as cru que c'était noël toute l'année ?");
+                return true;
+            }
+
+            // return error if the date is not valid
+            if (currentDay < day) {
+                player.sendMessage(Nowel.ERROR + "Patience, n'ouvre pas ton calendrier trop tôt ! :)");
+                return true;
+            }
+
+            // add web redirect
+            link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
+                    "http://xmas.onecraft.fr/" + day + ".php?uuid=" + player.getUniqueId().toString()
+            ));
+        }
+
+        TextComponent message = new TextComponent(Nowel.PREFIX + "Clique ");
+        message.setColor(ChatColor.GRAY);
+        message.addExtra(link);
+        message.addExtra(" pour obtenir l'indice !");
+
+        player.spigot().sendMessage(message);
+        return true;
+    }
+}
